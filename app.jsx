fd83ac871d0bb8319b1d4b56922e396631d27e89
Shannon Beach <shannonbeach1982@gmail.com>
2018-05-26 11:51:32 -0400

    refactored components

--- app.jsx
+++ app.jsx
@@ -1,23 +1,65 @@
-function Application() {
+function Header(props) {
+  return(
+      <div className="header">
+        <h1>{props.title}</h1>
+      </div>
+  );
+}
+
+Header.propTypes = {
+  title: React.PropTypes.string.isRequired,
+};
+
+function Player(props){
+  return(
+    <div className="player">
+      <div className="player-name">{props.name}</div>
+      <div className="player-score">
+        <Counter score={props.score} />
+      </div>
+    </div>
+  
+  );
+}
+
+Player.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  score: React.PropTypes.number.isRequired,
+};
+
+function Counter(props){
+  return(
+    <div className="counter">
+      <button className="counter-action decrement">-</button>
+      <div className="counter-score">{props.score}</div>
+      <button className="counter-action increment">+</button>
+    </div>
+  );
+};
+
+Counter.propTypes ={
+  score: React.PropTypes.number.isRequired,
+};
+
+
+function Application(props) {
   return (
     <div className="scoreboard">
-      <div className="header">
-        <h1>Scoreboard</h1>
-      </div>
+      <Header title={props.title}/> 
       <div className="players">
-        <div className="player">
-          <div className="player-name">Kermit</div>
-          <div className="score">
-            <div className="counter">
-              <button className="counter-action decrement">-</button>
-              <div className="counter-score">25</div>
-              <button className="counter-action increment">+</button>
-            </div>
-          </div>
-        </div>
+        <Player name="Kermit" score={25} />
+        <Player name="Gonzo" score={16} />
       </div>
     </div>
   );
 }
+
+Application.propTypes = {
+  title: React.PropTypes.string,
+};
+
+Application.defaultProps = {
+  title: "Scoreboard",
+};
 
 ReactDOM.render(<Application />, document.getElementById('container'));
