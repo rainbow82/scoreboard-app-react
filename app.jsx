143417fdc06a7145e1d11587659b1a8747f5c165
Shannon Beach <shannonbeach1982@gmail.com>
2018-05-27 11:19:24 -0400

    looping over the player list

--- app.jsx
+++ app.jsx
@@ -1,3 +1,21 @@
+var PLAYERS = [
+  {
+    name: "Kermit",
+    score: 25,
+    id: 1,
+  },
+    {
+    name: "Gonzo",
+    score: 16,
+    id: 2,
+  },
+    {
+    name: "Fonzi",
+    score: 12,
+    id: 3,
+  },
+]
+
 function Header(props) {
   return(
       <div className="header">
@@ -47,8 +65,9 @@ function Application(props) {
     <div className="scoreboard">
       <Header title={props.title}/> 
       <div className="players">
-        <Player name="Kermit" score={25} />
-        <Player name="Gonzo" score={16} />
+        {props.players.map(function(player){
+          return <Player name={player.name} score={player.score} key={player.id} />
+        })}
       </div>
     </div>
   );
@@ -56,10 +75,15 @@ function Application(props) {
 
 Application.propTypes = {
   title: React.PropTypes.string,
+  players: React.PropTypes.arrayOf(React.PropTypes.shape({
+    name: React.PropTypes.string.isRequired,
+    score: React.PropTypes.number.isRequired,
+    id: React.PropTypes.number.isRequired,
+  })).isRequired,
 };
 
 Application.defaultProps = {
   title: "Scoreboard",
 };
 
-ReactDOM.render(<Application />, document.getElementById('container'));
+ReactDOM.render(<Application players={PLAYERS} />, document.getElementById('container'));
